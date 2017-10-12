a44a2cdb265eb3a8fa4eb8c5e5d860f31dcca292
ConnorJang <connorjang20@gmail.com>
2017-10-12 09:27:40 -0700

    Moved models to data library, added references

--- ZenithDataLib/Models/AccountViewModels.cs
+++ ZenithDataLib/Models/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace ZenithWebSite.Models
+namespace ZenithDataLib.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
--- ZenithWebSite/Controllers/AccountController.cs
+++ ZenithWebSite/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using ZenithWebSite.Models;
+using ZenithDataLib.Models;
 
 namespace ZenithWebSite.Controllers
 {
