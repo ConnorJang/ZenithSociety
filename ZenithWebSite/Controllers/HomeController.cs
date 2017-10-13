14e527f275d4f0a8d71412c1cff8b7a396196e9e
Brayden Traas <btraas@gmail.com>
2017-10-13 13:44:02 -0700

    closes #21

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using ZenithDataLib.Models;
+using Microsoft.AspNet.Identity;
 
 namespace ZenithWebSite.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext dbContext = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            //DateTime oneWeek = DateTime.Now.AddDays(7);
+
+            int dayOffest = (int)DateTime.Now.DayOfWeek;
+            if (dayOffest == 0) dayOffest = 6;
+            else dayOffest--;
+
+
+
+            DateTime mondayDate = DateTime.Now.AddDays(-dayOffest);
+            DateTime sundayDate = DateTime.Now.AddDays(6 - dayOffest);
+
+            var events = dbContext.Events
+                .Where(x => x.ToDate.CompareTo(mondayDate) >= 0) // beginning of week
+                .Where(x => x.FromDate.CompareTo(sundayDate) <= 0)
+                .Include(x => x.ActivityCategory).ToList();
+
+            return View(events);
         }
 
         public ActionResult About()
