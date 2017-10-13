f30217655479234fbcd821480b4b3598ad315607
ConnorJang <connorjang20@gmail.com>
2017-10-13 14:23:01 -0700

    Added activityCategories views, authorized for admin only, link only shows for admins

--- ZenithDataLib/Models/ActivityCategory.cs
+++ ZenithDataLib/Models/ActivityCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace ZenithDataLib.Models
 {
     public class ActivityCategory
     {
+
         public int ActivityCategoryId { get; set; }
+        [Required]
+        [Display(Name = "Activity Description")]
         public string ActivityDescription { get; set; }
+        [Required]
+        [Display(Name = "Creation Date")]
         public DateTime CreationDate { get; set; }
     }
 }
